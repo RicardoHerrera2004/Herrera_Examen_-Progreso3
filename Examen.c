473a87bb4d39fa832d487b331bf96b0c3e62f26e
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2024-01-23 15:30:35 -0500

    Sexto commit: Documentacion y se pulio el codigo

--- Examen.c
+++ Examen.c
@@ -19,101 +19,13 @@ struct estudiante
 } alumno[];
 // Se crea la estructura de los alumnos
 
-void ingreso()
-{
-
-    int i;
-
-    char direccion[] = "D:\\Programacion RH\\3er_Progreso\\Herrera_Examen_-Progreso3\\alumnos.txt";
-    // Direccion del archivo, puede ser editable
-
-    archivo = fopen(direccion, "a");
-    // Se crea una asignacion para que los datos se vayan al archivo asignado
-
-    if (archivo == NULL)
-    {
-        printf("Error al ingresar en el archivo\n");
-        // Error si el arcihvo no se encuentra
-    }
-
-    printf("-Ingrese la matricula del estudiante-\n");
-    scanf("%d", &alumno[i].matricula);
-    // Ingreso tipo entero de la matricula dentro de la estructura alumno
-    fflush(stdin);
-    // Se limpia el buffer para no tener problemas al ingresar los datos
-
-    printf("-Ingrese el nombre del estudiante-\n");
-    gets(alumno[i].nombre);
-    // Ingreso del nombre del estudiante mediante la funcion gets en la estructura alumno
-    fflush(stdin);
-    // Se limpia el buffer para no tener problemas al ingresar los datos
-
-    printf("-Ingrese la direcion del estudiante-\n");
-    gets(alumno[i].direccion);
-    // Ingreso de la direccion del estudiante mediante la funcion gets en la estructura alumno
-    fflush(stdin);
-    // Se limpia el buffer para no tener problemas al ingresar los datos
-
-    printf("-Ingrese la materia del estudiante-\n");
-    gets(alumno[i].materia);
-    // Ingreso de la materia del estudiante mediante la funcion gets en la estructura alumno
-    fflush(stdin);
-    // Se limpia el buffer para no tener problemas al ingresar los datos
-
-    printf("-Ingrese la nota del estudiante-\n");
-    scanf("%f", &alumno[i].nota);
-    // Ingreso de tipo flotante de la nota del estudiante de la estructura del alumno
-    fflush(stdin);
-    // Se limpia el buffer para no tener problemas al ingresar los datos
-
-    printf("MATRICULA: %d\n", alumno[i].matricula);
-    printf("NOMBRE: %s\n", alumno[i].nombre);
-    printf("DIRECCION: %s\n", alumno[i].direccion);
-    printf("CARRERA: %s\n", alumno[i].materia);
-    printf("NOTA: %.2f\n", alumno[i].nota);
-    // Se imprime los valores ingresados recientemente
-
-    fprintf(archivo, "ESTUDIANTES MATRICULADOS\n");
-    fprintf("%d \t", alumno[i].matricula);
-    fprintf("%s \t", alumno[i].nombre);
-    fprintf("%s \t", alumno[i].direccion);
-    fprintf("%s \t", alumno[i].materia);
-
-    fclose(archivo);
-}
-// Funcion "ingreso" para ingresar las veces que sea, los estudiantes y sus diferentes datos: matricula, nombre,etc.
-
-void visualizar()
-{
-    char direccion[] = "D:\\Programacion RH\\3er_Progreso\\Herrera_Examen_-Progreso3\\alumnos.txt";
-    // Direccion del archivo, puede ser editable
-
-    int ayudante;
-    archivo = fopen(direccion, "r");
-    if (archivo == NULL)
-    {
-        printf("Error al ingresar en el archivo\n");
-    }
-    while ((ayudante = fgetc(archivo)) != EOF)
-    {
-        if (ayudante == '\n')
-        {
-            printf("\n");
-        }
-        else
-        {
-            putchar(ayudante);
-        }
-    }
-    fclose(archivo);
-}
-
 void agregar()
 {
     char direccion[] = "D:\\Programacion RH\\3er_Progreso\\Herrera_Examen_-Progreso3\\alumnos.txt";
     // Direccion del archivo, puede ser editable
 
     int respuesta, i = 1;
+    //Se asigna las variables "respuesta" para pedir al usiario cerrar el bucle do-while y la de "i" para contar los alumnos ingresados
 
     archivo = fopen(direccion, "a");
     // Se crea una asignacion para que los datos se vayan al archivo asignado
@@ -121,10 +33,8 @@ void agregar()
     if (archivo == NULL)
     {
         printf("Error al ingresar en el archivo\n");
-        //Error si no encuentra el archivo que se quiere ingresar los datos
+        // Error si no encuentra el archivo que se quiere ingresar los datos
     }
-    printf("Creando base de datos\n");
-    fprintf(archivo, "ESTUDIANTES MATRICULADOS\n");
 
     do
     {
@@ -143,6 +53,8 @@ void agregar()
         printf("-Ingrese la nota del estudiante-\n");
         scanf("%f", &alumno[i].nota);
         fflush(stdin);
+        // Se pide al usuario que ingrese los datos pertientes para cada variable de la estructura
+        // Ademas, se utiliza el fflush para limpiar el buffer y no tener problemas al ingreso de datos
 
         printf("MATRICULA: %d\n", alumno[i].matricula);
         printf("NOMBRE: %s\n", alumno[i].nombre);
@@ -151,18 +63,50 @@ void agregar()
         printf("NOTA: %.2f\n", alumno[i].nota);
         // Se imprime los valores ingresados recientemente
 
-        fprintf(archivo, "%d\t", alumno[i].matricula);
-        fprintf(archivo, "%s\t", alumno[i].nombre);
-        fprintf(archivo, "%s\t", alumno[i].direccion);
-        fprintf(archivo, "%s\t", alumno[i].materia);
+        fprintf(archivo, "%d\t\t", alumno[i].matricula);
+        fprintf(archivo, "%s\t\t", alumno[i].nombre);
+        fprintf(archivo, "%s\t\t", alumno[i].direccion);
+        fprintf(archivo, "%s\t\t", alumno[i].materia);
         fprintf(archivo, "%.2f\n", alumno[i].nota);
 
         printf("¿Desea salir? (1. para salir, Cualquier otro numero para continuar)\n");
         scanf("%d", &respuesta);
-        getchar();
+        // Se pregunta al usuario si desea salir del bucle de ingreso de estudiantes
+
     } while (respuesta != 1);
-    //Se crea un bucle dowhile para el conteo de cuantos alumnos se esta ingresando
-    
+    // Se crea un bucle do-while para el conteo de cuantos alumnos se esta ingresando
+
+    fclose(archivo);
+    // Se cierra el archivo
+}
+
+void visualizar()
+{
+    char direccion[] = "D:\\Programacion RH\\3er_Progreso\\Herrera_Examen_-Progreso3\\alumnos.txt";
+    // Direccion del archivo, puede ser editable
+
+    int ayudante;
+    // Asignacion de la variable entera ayudante
+
+    archivo = fopen(direccion, "r");
+    // Se abre el archivo y se asigna la direccion "r" de read
+
+    if (archivo == NULL)
+    {
+        printf("Error al ingresar en el archivo\n");
+        // Error si no encuentra el archivo que se quiere ingresar los datos
+    }
+    while ((ayudante = fgetc(archivo)) != EOF)
+    {
+        if (ayudante == '\n')
+        {
+            printf("\n");
+        }
+        else
+        {
+            putchar(ayudante);
+        }
+    }
     fclose(archivo);
     //Se cierra el archivo
 }
@@ -171,6 +115,7 @@ int main()
 {
 
     int boton;
+    // Se asigna la variable boto para el uso de la funcion switch
 
     do
     {
@@ -179,24 +124,34 @@ int main()
         printf("Ingrese 2. para mostrar a los estudiantes ingresados\n");
         printf("Ingrese 3. para salir\n");
         scanf("%d", &boton);
+        //Se muetras las opciones al usuario y le pide que ingrese la opcion
+
         switch (boton)
         {
         case 1:
             printf("-Ingreso de estudiantes-\n");
             agregar();
+            //En el caso 1 se usa la funcion agregar para la asignacion de datos de estudiantes
+
             break;
 
         case 2:
             printf("-Muestreo de estudiantes-\n");
             visualizar();
+            //En el caso 2 se usa la funcion visualizar para ver los estudiantes que esten agregados de manera horizontal
+
             break;
 
         case 3:
             printf("-Gracias por usar el programa-\n");
+            //Se agradece al usuario de usar el programa y se cerrara el programa en el caso 3
+
             break;
 
         default:
             printf("-Error al ingresar el numero, intente de nuevo-\n");
+            //Salta un error si el usuario no ingresa los numeros pedidos
+
             break;
         }
     } while (boton != 3);
